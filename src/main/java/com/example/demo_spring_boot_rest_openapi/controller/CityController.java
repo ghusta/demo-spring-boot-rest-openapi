3dcbd78d3d531ea8ac2da2d9941e055f2963afb0
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-10-25 09:05:45 +0200

    feat: Add CityController.getAllCitiesPaginated for pagination

--- src/main/java/com/example/demo_spring_boot_rest_openapi/controller/CityController.java
+++ src/main/java/com/example/demo_spring_boot_rest_openapi/controller/CityController.java
@@ -4,6 +4,8 @@ import com.example.demo_spring_boot_rest_openapi.dao.CityRepository;
 import com.example.demo_spring_boot_rest_openapi.exception.ResourceNotFoundException;
 import com.example.demo_spring_boot_rest_openapi.persistence.City;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +24,18 @@ public class CityController {
         this.cityRepository = cityRepository;
     }
 
-    @GetMapping
+    /**
+     * @deprecated Prefer paginated version
+     */
+    @GetMapping("/unpaginated")
+    @Deprecated
     public List<City> getAllCities() {
         return cityRepository.findAll();
+    }
+
+    @GetMapping
+    public Page<City> getAllCitiesPaginated(Pageable pageable) {
+        return cityRepository.findAll(pageable);
     }
 
     @GetMapping("/{id}")
